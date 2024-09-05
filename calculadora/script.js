c19dbd030af0fb9b6561a996391b2b566c1dcd60
Nicolas Inoue <144853890+Nic-Inoue@users.noreply.github.com>
2024-09-05 17:29:32 -0300

    Add files via upload
    Made a function to identify a operator as a last character

--- calculadora/script.js
+++ calculadora/script.js
@@ -4,16 +4,20 @@ function appendToDisplay(input) {
   display.value += input;
  }
 
-function backspace(input) {
+function backspace() {
   display.value = display.value.slice(0, display.value.length -1);
 }
+
+function isOperator(digit) {
+  return ["/","*","+","-"].includes(digit);
+}
 function appendToDisplayOperator(input){
-  if (display.value.slice(-1) === "+" || display.value.slice(-1) === "-" || display.value.slice(-1) === "/" || display.value.slice(-1) === "*"){
+  if (isOperator(display.value.slice(-1))){
       display.value = display.value.slice(0, display.value.length -1);
   }
   if (display.value !== "") {
     display.value += input;
-}
+  }
 }
 
 function clearDisplay(){
@@ -21,14 +25,14 @@ function clearDisplay(){
 }
 
 function calculate(){
-  if (display.value.slice(-1) === "+" || display.value.slice(-1) === "-" || display.value.slice(-1) === "/" || display.value.slice(-1) === "*"){
+  if (isOperator(display.value.slice(-1))){
     display.value = display.value.slice(0, display.value.length -1 );
-  }
+ }
   display.value = eval(display.value)
 }
 
 function squared(){
-    if (display.value.slice(-1) === "+" || display.value.slice(-1) === "-" || display.value.slice(-1) === "/" || display.value.slice(-1) === "*"){
+    if (isOperator(display.value.slice(-1))){
         display.value = display.value.slice(0, display.value.length -1);
     }
     calculate(display.value);
